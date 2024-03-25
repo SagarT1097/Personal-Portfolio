9b4a8266f968520e27c5dcaed0d8bcd3ee0bbbfc
Sagar Tambrahalli <sagar.uu97@gmail.com>
2024-03-25 12:22:32 +0100

    Commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,48 +1,80 @@
-import React from 'react'
-import {motion } from "framer-motion"
-import { urlFor } from '../../sanity'
-import Image from 'next/image'
+import React from "react";
+import { motion } from "framer-motion";
+import { urlFor } from "../../sanity";
+import Image from "next/image";
 
-import abtImg  from '../assets/abt.png'
-type Props = { }
+import abtImg from "../assets/abt.png";
+type Props = {};
 
-export default function About({ }: Props) {
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{duration:1.5}}
-            className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-            <h3 className="absolute uppercase tracking-[20px] text-gray-500 text-sm md:text-2xl top-16 md:top-14">
-            About
-            </h3>
-            <motion.div/>
-            <Image
-                         width={500}
-                         height={500}
-                        src={abtImg}
-                         alt="Volvo Cars"
-                         className="-mb-20 md:mb-0 p-20 md:p-0 flex-shrink-0 w-56 h-56 object-contain xl:w-[350px] xl:h-[350px]"
-                        />
-           
+export default function About({}: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute uppercase tracking-[20px] text-gray-500 text-sm md:text-2xl top-16 md:top-14">
+        About
+      </h3>
+      <motion.div />
+      <Image
+        width={500}
+        height={500}
+        src={abtImg}
+        alt="Volvo Cars"
+        className="-mb-20 md:mb-0 p-20 md:p-0 flex-shrink-0 w-56 h-56 object-contain xl:w-[350px] xl:h-[350px]"
+      />
 
-            <div className="md:px-12">
-                <h4 className="text-2xl font-semibold md:text-3xl">
-                    A <span className="underline decoration-[#0F4C75]/50">brief</span> background</h4>
-            </div>
-            <div className="text-sm md:text-sm xl:text-xl">
-                <p>
-                    Recent Computer Science Masters graduate from Uppsala University. I am passionate about XR, Full-Stack development, and Computer Vision.  At Volvo Cars, I delved into XR research, while crafting a Customer interaction analysis tool using Microsoft Hololens 2, Unity3D and C#.
-                </p>
-                <br/>
-                <p>
-                    My thesis explored XR applications in the Automobile industry, showcasing XR development, Unity3D, C#, and problem-solving skills. Practical experience includes Full-stack development and Project Engineering, with expertise in React, Django and .NET.
-                </p>
-                <br/>
-                    <p>
-                    Proficient in Python, Java, MATLAB, Figma and Blender as well. Always eager to learn and contribute to innovative XR solutions, my goal is to make an impact in XR software development. 
-                </p>   
-</div>
-        </motion.div>
-    )
+      <div className="md:px-12">
+        <h4 className="text-2xl font-semibold md:text-3xl">
+          A <span className="underline decoration-[#0F4C75]/50">brief</span>{" "}
+          background
+        </h4>
+      </div>
+      <div className="text-sm md:text-sm xl:text-md">
+        <p>
+          I am a versatile and dedicated Software Engineer specializing in the
+          development of Software as a Service applications using Next.js. With
+          a strong foundation in both frontend and backend technologies, I excel
+          in crafting robust and scalable solutions that meet the dynamic needs
+          of modern businesses. My expertise lies in leveraging a diverse skill
+          set, including React, TypeScript, Node.js, PostgreSQL, and AWS, to
+          deliver cutting-edge applications that drive innovation and
+          efficiency.
+        </p>
+        <br />
+        <p>
+          My journey in software development has been marked by impactful
+          projects and enriching experiences. At Volvo Cars, I spearheaded the
+          design and development of a groundbreaking 3D Data Visualization tool,
+          utilizing C#, Unity3D, and Microsoft Hololens 2, to analyze customer
+          interactions and inform product decisions. This experience honed my
+          ability to collaborate effectively across departments and deliver
+          solutions that align with strategic objectives.
+        </p>
+        <br />
+        <p>
+          My passion for innovation extends beyond traditional software
+          development, as evidenced by my involvement in projects such as
+          CerebralAI, EduPod, and AI Chatbot, which integrate advanced AI
+          technologies to deliver cutting-edge solutions. Furthermore, my
+          educational background, including a Master of Science in Computer
+          Science from Uppsala University and a Bachelor of Engineering from
+          NITTE Meenakshi Institute of Technology, has equipped me with a
+          comprehensive understanding of computer science principles and
+          emerging technologies.
+        </p>
+        <br />
+        <p>
+          As a language-agnostic problem solver with excellent communication and
+          collaboration skills, I thrive in dynamic environments where
+          creativity and teamwork are valued. Whether it's developing intuitive
+          user interfaces or architecting scalable backend systems, I am
+          committed to delivering high-quality solutions that exceed
+          expectations and drive business success.
+        </p>
+      </div>
+    </motion.div>
+  );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,52 +1,60 @@
-import React from 'react'
-import { Cursor, useTypewriter } from 'react-simple-typewriter'
-import BackgroundCircle from "../components/BackgroundCircle"
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircle from "../components/BackgroundCircle";
+import Image from "next/image";
+import Link from "next/link";
 
-import { urlFor } from '../../sanity'
-import heroImg from '../assets/heroImg.jpg'
+import { urlFor } from "../../sanity";
+import heroImg from "../assets/heroImg.jpg";
 
-type Props = {}
+type Props = {};
 
-export default function Hero({ }: Props) {
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hej! I'm Sagar Tambrahalli.",
+      "Engineering Code for SaaS Applications.",
+      "Explore my portfolio for more!",
+    ],
+    loop: true,
+    delaySpeed: 1000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircle />
+      <Image
+        className="relative rounded-full mx-auto object-cover"
+        src={heroImg}
+        width={100}
+        height={100}
+        alt=""
+      />
 
-    const [text, count] = useTypewriter({
-        words: [
-            "Hej! I'm Sagar Tambrahalli.",
-            "Engineering Code for Web and Immersive XR Realms.",
-            "Explore my portfolio for more!",
-        ],
-        loop: true,
-        delaySpeed:1000
-    })
-    return (
-        <div className = "h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-            <BackgroundCircle />
-            <Image
-                className="relative rounded-full mx-auto object-cover"
-                src={heroImg}
-                width={100}
-                height={100}
-                alt=""
-            />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-500 tracking-[15px]">
+          Software Engineer
+        </h2>
 
-            <div className="z-20">
-                <h2 className="text-sm uppercase text-gray-500 pb-500 tracking-[15px]">Software Engineer</h2>
-            
-            <h1 className ="text-5xl lg:text-6xl font-semibold px-10 ">
-                <span>{text}</span>
-                <Cursor cursorColor="#0F4C75"></Cursor>
-            </h1>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10 ">
+          <span>{text}</span>
+          <Cursor cursorColor="#0F4C75"></Cursor>
+        </h1>
 
-            <div className="pt-5">
-                <Link href="#about"><button className="heroButton">About</button></Link> 
-                <Link href="#experience"><button className="heroButton">Experience</button></Link>
-                <Link href="#skills"><button className="heroButton">Skills</button></Link>
-                <Link href="#projects"><button className="heroButton">Projects</button></Link>
-            </div>
-            </div>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
         </div>
-
-         )
+      </div>
+    </div>
+  );
 }
